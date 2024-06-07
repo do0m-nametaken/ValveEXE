8e241de061c780655e38071356683b1822942f2d
Do0m <59143273+do0m-nametaken@users.noreply.github.com>
2024-06-07 09:07:04 +0800

    Tweak and add docs to logfile.py
    Renamed LogFile.function to LogFile.callback

--- valveexe/logfile.py
+++ valveexe/logfile.py
@@ -4,16 +4,22 @@ from watchdog.events import FileSystemEventHandler
 from os.path import dirname, abspath
 
 class LogFile:
+    '''
+    Initialize a console logfile to track.
+    '''
     def __init__(self, file_path):
+        '''
+        :param file_path: The path to the log file
+        :type file_path: path, str
+         '''
         self.file_path = file_path
-        self.logs = ""
+        self.logs = ""  #: :type: (str) - all the logs accumulated so far
         self._bookmark = 0
-        self.watchers = []
 
     def ingest(self):
         '''
         Will resume reading the logs from where it last left off until the end
-        and return all logs since
+        and return all logs since.
 
         :rtype: str
         '''
@@ -27,30 +33,60 @@ class LogFile:
         return logs_since_bookmark
 
 class LogWatch():
+    '''
+    An abstract class that can automatically detect changes in a LogFile.
+    Does nothing on its own. 
+    '''
     def __init__(self, logfile):
+        '''
+        :param logfile: The LogFile to watch
+        :type logfile: LogFile
+        '''
         self.logfile = logfile
         self._event_handler = FileSystemEventHandler()
 
     def start(self):
-        self.logfile.watchers.append(self)
+        '''
+        Start watching the LogFile.
+        '''
         self._observer = Observer()
         self._observer.daemon = True
-        self._observer.schedule(self._event_handler, dirname(self.logfile.file_path), recursive=False)
+        self._observer.schedule(
+            self._event_handler,
+            dirname(self.logfile.file_path),
+            recursive=False)
         self._observer.start()
 
     def stop(self):
-        self.logfile.watchers.remove(self)
+        '''
+        Stop watching the LogFile.
+        '''
         self._observer.stop()
         del self._observer
 
 class RegexLogWatch(LogWatch):
-    def __init__(self, logfile, regex, function=None, *function_args):
+    '''
+    Log Watcher that will :any:`ingest()<ingest>` whenever the LogFile is
+    modified and will execute the specified callback if a specified regex
+    is found within the logfile.
+    '''
+    def __init__(self, logfile, regex, callback=None, *callback_args):
+        '''
+        :param logfile: The LogFile to watch
+        :type logfile: LogFile
+        :param regex: A regex string to match against the logs
+        :type regex: str
+        :param callback: The function to run on a successfui regex match
+        :type callback: function
+        :param *callback_args: The arguments to pass to the callback
+        function
+        '''
         super().__init__(logfile)
         self.regex = regex
-        self.function = function
-        self.function_args = []
-        for arg in function_args:
-            self.function_args.append(arg)
+        self.callback = callback
+        self.callback_args = []
+        for arg in callback_args:
+            self.callback_args.append(arg)
         self._event_handler.on_modified = self._on_modified
 
     def _on_modified(self, event):
@@ -58,8 +94,7 @@ class RegexLogWatch(LogWatch):
                 not event.is_directory and
                 abspath(event.src_path) == abspath(self.logfile.file_path) and
                 regexsearch(self.regex, self.logfile.ingest()) and
-                self.function is not None and callable(self.function)
-            ):
-            self.function(*self.function_args)
+                self.callback is not None and callable(self.callback)):
+            self.callback(*self.callback_args)
         else:
             pass
